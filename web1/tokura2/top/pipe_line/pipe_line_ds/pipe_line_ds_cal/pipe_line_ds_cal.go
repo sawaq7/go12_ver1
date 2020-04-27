4a6e02fa37d41240bdda74d1376022df0e10c08c
sawaq <60250540+sawaq7@users.noreply.github.com>
2020-04-27 23:52:35 +0900

    add new object

--- web1/tokura2/top/pipe_line/pipe_line_ds/pipe_line_ds_cal/pipe_line_ds_cal.go
+++ web1/tokura2/top/pipe_line/pipe_line_ds/pipe_line_ds_cal/pipe_line_ds_cal.go
@@ -8,6 +8,7 @@ import (
 	    "github.com/sawaq7/go12_ver1/client/tokura/suiri/cal"
 	    "github.com/sawaq7/go12_ver1/client/tokura/suiri/type4"
 	    "github.com/sawaq7/go12_ver1/client/tokura/datastore4"
+	    "github.com/sawaq7/go12_ver1/basic/type3"
 //	    "strconv"
 //	    "fmt"
 
@@ -107,18 +108,23 @@ func Pipe_line_ds_cal(w http.ResponseWriter, r *http.Request) {
 
 /// グラフの作成 ///
 
-    p_number := 1
-    ad_eneup := make([]type3.Point ,20 ,50)
-   ad_enedown := make([]type3.Point ,20 ,50)
-   ad_glineup := make([]type3.Point ,20 ,50)
-   ad_glinedown := make([]type3.Point ,20 ,50)
+//    p_number := 1
+//    ad_eneup := make([]type3.Point ,20 ,50)
+//   ad_enedown := make([]type3.Point ,20 ,50)
+//   ad_glineup := make([]type3.Point ,20 ,50)
+//   ad_glinedown := make([]type3.Point ,20 ,50)
 
-    f_name := cal.Pipe_line1_make_graf( w ,r ,p_number ,ad_eneup ,ad_enedown ,ad_glineup ,ad_glinedown  )
+//   ad_eneup[0].X = 1.
+//      ad_enedown[0].Y = 1.
+//      ad_glineup[0].X = 1.
+//      ad_glinedown[0].Y = 1.
+
+//    f_name := cal.Pipe_line1_make_graf( w ,r ,p_number ,ad_eneup ,ad_enedown ,ad_glineup ,ad_glinedown  )
 
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : f_name %v\n", f_name )  // デバック
 
 /// グラフの表示 ///
-
+    f_name := "1"
     cal.Pipe_line1_show_graf( w ,r ,f_name )
 
 }
