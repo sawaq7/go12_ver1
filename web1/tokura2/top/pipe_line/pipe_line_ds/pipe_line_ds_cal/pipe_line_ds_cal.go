7b6374072318c05b27b31eb4c7b634713d5eb274
sawaq <60250540+sawaq7@users.noreply.github.com>
2020-04-30 00:41:05 +0900

    various access

--- web1/tokura2/top/pipe_line/pipe_line_ds/pipe_line_ds_cal/pipe_line_ds_cal.go
+++ web1/tokura2/top/pipe_line/pipe_line_ds/pipe_line_ds_cal/pipe_line_ds_cal.go
@@ -4,16 +4,14 @@ import (
 
 	    "net/http"
 	    "github.com/sawaq7/go12_ver1/client/tokura/suiri/cal"
-//	    "github.com/sawaq7/go12_ver1/client/tokura/suiri/type4"
-
-//	    "github.com/sawaq7/go12_ver1/client/tokura/datastore4/trans2"
-//	    "github.com/sawaq7/go12_ver1/basic/type3"
+	    "github.com/sawaq7/go12_ver1/client/tokura/suiri/type4"
+	    "github.com/sawaq7/go12_ver1/client/tokura/datastore4"
 //	    "strconv"
-	    "fmt"
+//	    "fmt"
 
-//        "cloud.google.com/go/datastore"
-//        "context"
-//        "os"
+        "cloud.google.com/go/datastore"
+        "context"
+        "os"
 
                                                   )
 
@@ -21,13 +19,79 @@ func Pipe_line_ds_cal(w http.ResponseWriter, r *http.Request) {
 
 //   fmt.Fprintf( w, "sky/pipe_line_ds_cal start \n"  )
 
+///    get key-in data
+
+   var water type4.Water2
+
+   project_name := os.Getenv("GOOGLE_CLOUD_PROJECT")
+
+    if project_name == "" {
+
+      project_name = "sample-7777"
+
+	}
+
+    ctx := context.Background()
+
+    client, err := datastore.NewClient(ctx, project_name)
+
+    query := datastore.NewQuery("Water2_Temp").Order("Name")
+
+    count, err := client.Count(ctx, query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+//    fmt.Fprintf( w, "sky/pipe_line_ds_cal  \n" ,count )
+
+    water_temp      := make([]type4.Water2, 0, count)
+
+    keys, err := client.GetAll(ctx, query , &water_temp)
+/
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+    }
+
+//    fmt.Fprintf( w, "sky/pipe_line_ds_cal : len(water) %v\n", len(water) )
+
+    keys_wk := make([]int64, count)
+
+	for ii, keysw := range keys {
+
+       keys_wk[ii] = keysw.ID
+
+    }
+
+	for pos2, waterw := range water_temp {
+
+       water.Id   = keys_wk[pos2]
+
+	   water.Name = waterw.Name
+	   water.High =  waterw.High
+	   water.Roughness_Factor = waterw.Roughness_Factor
+
+    }
+
 ///
-///
+///      get water-line inf.
 ///
 
-//      p_number ,ad_eneup ,ad_enedown ,ad_glineup ,ad_glinedown := cal4.Pipe_line1( w  ,r  )
+//    water_line := trans2.Water_line (1  ,water.Name , w ,r )
 
-//     p_number  := 1
+      water_line := datastore4.Datastore_tokura( "Water_Line"  ,"trans"  ,water.Name , w , r  )
+
+
+
+     value, _ := water_line.([]type4.Water_Line)
+
+///
+///        cal. water-slope-line
+///
+
+    p_number ,ad_eneup ,ad_enedown ,ad_glineup ,ad_glinedown := cal.Pipe_line1( water  ,value  )
+
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : p_number %v\n", p_number )
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : ad_eneup %v\n", ad_eneup )
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : ad_enedown %v\n", ad_enedown )
@@ -35,25 +99,15 @@ func Pipe_line_ds_cal(w http.ResponseWriter, r *http.Request) {
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : ad_glinedown %v\n", ad_glinedown )
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : ad_eneup len %v\n", len(ad_eneup) )
 
-/// グラフの作成 ///
+///    make graf
 
-//    p_number := 1
-//    ad_eneup := make([]type3.Point ,20 ,50)
-//   ad_enedown := make([]type3.Point ,20 ,50)
-//   ad_glineup := make([]type3.Point ,20 ,50)
-//   ad_glinedown := make([]type3.Point ,20 ,50)
-
-//   ad_eneup[0].X = 1.
-//      ad_enedown[0].Y = 1.
-//      ad_glineup[0].X = 1.
-//      ad_glinedown[0].Y = 1.
-
-//    f_name := cal.Pipe_line1_make_graf( w ,r ,p_number ,ad_eneup ,ad_enedown ,ad_glineup ,ad_glinedown  )
+    f_name := cal.Pipe_line1_make_graf( w ,r ,p_number ,ad_eneup ,ad_enedown ,ad_glineup ,ad_glinedown  )
 
 //    fmt.Fprintf( w, "sky/pipe_line_ds_cal : f_name %v\n", f_name )
 
-///
-    f_name := "1"
+///     show graf on web
+
     cal.Pipe_line1_show_graf( w ,r ,f_name )
 
 }
+
